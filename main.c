f044a7cbf5a95c215ac02f70ce669dff30f71746
danebidev <57720273+danebidev@users.noreply.github.com>
2024-10-20 12:37:20 +0200

    Moved a bit of stuff

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
+#include <config.h>
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 typedef struct {
-    char *program_name;
     char *config_file;
     char *data_dir;
 } config_t;
@@ -16,9 +16,11 @@ void die() {
 }
 
 void config_init() {
-    config.program_name = "ewine";
     config.config_file = "${XDG_CONFIG_HOME}/ewine/config.toml";
     config.data_dir = "${XDG_DATA_HOME}/ewine";
+}
+
+void config_load() {
 }
 
 int main(int argc, char *argv[]) {
@@ -28,16 +30,13 @@ int main(int argc, char *argv[]) {
 
     while ((opt = getopt(argc, argv, "c:")) != -1) {
         switch (opt) {
-            case 'c':
-                config.config_file = optarg;
-                break;
             case '?':
-                // printf("Try '%s --help' for more information", config.program_name);
+                printf("Try '%s --help' for more information", PROGRAM_NAME);
                 die();
         }
     }
 
     if (access(config.config_file, R_OK) != -1) {
-        printf("config file not found. Please create it");
+        config_load();
     }
 }
