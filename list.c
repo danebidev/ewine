6eb15eeeb4c3eec64906a225ac8e6c2eb7610d1e
danebidev <57720273+danebidev@users.noreply.github.com>
2024-11-02 16:57:31 +0100

    Fixed strcmp check in list

--- list.c
+++ list.c
@@ -7,7 +7,7 @@
 #include "data.h"
 
 void list_prefixes() {
-    printf("Prefixes\n");
+    if (data.prefixes) printf("Prefixes\n");
     for (int cur_prefix = 0; cur_prefix < data.prefix_count; cur_prefix++) {
         printf("    Name: %s\n", data.prefixes[cur_prefix].name);
         printf("    Path: %s\n", data.prefixes[cur_prefix].path);
@@ -20,7 +20,7 @@ void list_prefixes() {
 }
 
 void list_wine_installs() {
-    printf("Wine\n");
+    if (data.wine_count) printf("Wine\n");
     for (int cur_wine = 0; cur_wine < data.wine_count; cur_wine++) {
         printf("    Name: %s\n", data.wine_installs[cur_wine].name);
         printf("    Path: %s\n", data.wine_installs[cur_wine].path);
@@ -29,7 +29,7 @@ void list_wine_installs() {
 }
 
 void list_dxvk_installs() {
-    printf("DXVK\n");
+    if (data.dxvk_count) printf("DXVK\n");
     for (int cur_dxvk = 0; cur_dxvk < data.dxvk_count; cur_dxvk++) {
         printf("    Name: %s\n", data.dxvk_installs[cur_dxvk].name);
         printf("    Path: %s\n", data.dxvk_installs[cur_dxvk].path);
@@ -41,7 +41,7 @@ void list_dxvk_installs() {
  * @return 0 if the command completed succesfully, -1 otherwise
  */
 int command_list(char *argv[], int argc, int args_index) {
-    if (args_index++ >= argc) {
+    if (args_index >= argc) {
         list_prefixes();
         printf("\n");
         list_wine_installs();
@@ -50,14 +50,14 @@ int command_list(char *argv[], int argc, int args_index) {
     }
     else {
         for (; args_index < argc; args_index++) {
-            if (strcmp(argv[args_index], "prefix"))
+            if (strcmp(argv[args_index], "prefix") == 0)
                 list_prefixes();
-            else if (strcmp(argv[args_index], "wine"))
+            else if (strcmp(argv[args_index], "wine") == 0)
                 list_wine_installs();
-            else if (strcmp(argv[args_index], "dxvk"))
+            else if (strcmp(argv[args_index], "dxvk") == 0)
                 list_dxvk_installs();
             else {
-                printf("%s: Unrecognized component type '%s'. Should be one of 'prefix', 'wine' or 'dxvk'", PROGRAM_NAME, argv[args_index]);
+                printf("%s: Unrecognized component type '%s'. Should be one of 'prefix', 'wine' or 'dxvk'\n", PROGRAM_NAME, argv[args_index]);
                 return -1;
             }
         }
